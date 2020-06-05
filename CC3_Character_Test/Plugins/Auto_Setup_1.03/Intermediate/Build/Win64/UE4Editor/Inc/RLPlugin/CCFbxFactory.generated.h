c3835477e618c1b8b19a430cceff1bd0e72283d9
somnbody <somnbody@users.noreply.github.com>
2020-06-05 19:36:07 -0400

    Initial commit.

---
+++ CC3_Character_Test/Plugins/Auto_Setup_1.03/Intermediate/Build/Win64/UE4Editor/Inc/RLPlugin/CCFbxFactory.generated.h
@@ -0,0 +1,95 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef RLPLUGIN_CCFbxFactory_generated_h
+#error "CCFbxFactory.generated.h already included, missing '#pragma once' in CCFbxFactory.h"
+#endif
+#define RLPLUGIN_CCFbxFactory_generated_h
+
+#define C__Users_allenlee_BEYOUNG_Desktop_Unreal_4_24_RLLiveLink_RLLiveLink_RLPlugin_HostProject_Plugins_RLPlugin_Source_RLPlugin_Private_CCFbxFactory_h_21_SPARSE_DATA
+#define C__Users_allenlee_BEYOUNG_Desktop_Unreal_4_24_RLLiveLink_RLLiveLink_RLPlugin_HostProject_Plugins_RLPlugin_Source_RLPlugin_Private_CCFbxFactory_h_21_RPC_WRAPPERS
+#define C__Users_allenlee_BEYOUNG_Desktop_Unreal_4_24_RLLiveLink_RLLiveLink_RLPlugin_HostProject_Plugins_RLPlugin_Source_RLPlugin_Private_CCFbxFactory_h_21_RPC_WRAPPERS_NO_PURE_DECLS
+#define C__Users_allenlee_BEYOUNG_Desktop_Unreal_4_24_RLLiveLink_RLLiveLink_RLPlugin_HostProject_Plugins_RLPlugin_Source_RLPlugin_Private_CCFbxFactory_h_21_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesUCCFbxFactory(); \
+	friend struct Z_Construct_UClass_UCCFbxFactory_Statics; \
+public: \
+	DECLARE_CLASS(UCCFbxFactory, UFbxFactory, COMPILED_IN_FLAGS(0), CASTCLASS_None, TEXT("/Script/RLPlugin"), NO_API) \
+	DECLARE_SERIALIZER(UCCFbxFactory)
+
+
+#define C__Users_allenlee_BEYOUNG_Desktop_Unreal_4_24_RLLiveLink_RLLiveLink_RLPlugin_HostProject_Plugins_RLPlugin_Source_RLPlugin_Private_CCFbxFactory_h_21_INCLASS \
+private: \
+	static void StaticRegisterNativesUCCFbxFactory(); \
+	friend struct Z_Construct_UClass_UCCFbxFactory_Statics; \
+public: \
+	DECLARE_CLASS(UCCFbxFactory, UFbxFactory, COMPILED_IN_FLAGS(0), CASTCLASS_None, TEXT("/Script/RLPlugin"), NO_API) \
+	DECLARE_SERIALIZER(UCCFbxFactory)
+
+
+#define C__Users_allenlee_BEYOUNG_Desktop_Unreal_4_24_RLLiveLink_RLLiveLink_RLPlugin_HostProject_Plugins_RLPlugin_Source_RLPlugin_Private_CCFbxFactory_h_21_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API UCCFbxFactory(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(UCCFbxFactory) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, UCCFbxFactory); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(UCCFbxFactory); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API UCCFbxFactory(UCCFbxFactory&&); \
+	NO_API UCCFbxFactory(const UCCFbxFactory&); \
+public:
+
+
+#define C__Users_allenlee_BEYOUNG_Desktop_Unreal_4_24_RLLiveLink_RLLiveLink_RLPlugin_HostProject_Plugins_RLPlugin_Source_RLPlugin_Private_CCFbxFactory_h_21_ENHANCED_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API UCCFbxFactory(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()) : Super(ObjectInitializer) { }; \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API UCCFbxFactory(UCCFbxFactory&&); \
+	NO_API UCCFbxFactory(const UCCFbxFactory&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, UCCFbxFactory); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(UCCFbxFactory); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(UCCFbxFactory)
+
+
+#define C__Users_allenlee_BEYOUNG_Desktop_Unreal_4_24_RLLiveLink_RLLiveLink_RLPlugin_HostProject_Plugins_RLPlugin_Source_RLPlugin_Private_CCFbxFactory_h_21_PRIVATE_PROPERTY_OFFSET
+#define C__Users_allenlee_BEYOUNG_Desktop_Unreal_4_24_RLLiveLink_RLLiveLink_RLPlugin_HostProject_Plugins_RLPlugin_Source_RLPlugin_Private_CCFbxFactory_h_18_PROLOG
+#define C__Users_allenlee_BEYOUNG_Desktop_Unreal_4_24_RLLiveLink_RLLiveLink_RLPlugin_HostProject_Plugins_RLPlugin_Source_RLPlugin_Private_CCFbxFactory_h_21_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	C__Users_allenlee_BEYOUNG_Desktop_Unreal_4_24_RLLiveLink_RLLiveLink_RLPlugin_HostProject_Plugins_RLPlugin_Source_RLPlugin_Private_CCFbxFactory_h_21_PRIVATE_PROPERTY_OFFSET \
+	C__Users_allenlee_BEYOUNG_Desktop_Unreal_4_24_RLLiveLink_RLLiveLink_RLPlugin_HostProject_Plugins_RLPlugin_Source_RLPlugin_Private_CCFbxFactory_h_21_SPARSE_DATA \
+	C__Users_allenlee_BEYOUNG_Desktop_Unreal_4_24_RLLiveLink_RLLiveLink_RLPlugin_HostProject_Plugins_RLPlugin_Source_RLPlugin_Private_CCFbxFactory_h_21_RPC_WRAPPERS \
+	C__Users_allenlee_BEYOUNG_Desktop_Unreal_4_24_RLLiveLink_RLLiveLink_RLPlugin_HostProject_Plugins_RLPlugin_Source_RLPlugin_Private_CCFbxFactory_h_21_INCLASS \
+	C__Users_allenlee_BEYOUNG_Desktop_Unreal_4_24_RLLiveLink_RLLiveLink_RLPlugin_HostProject_Plugins_RLPlugin_Source_RLPlugin_Private_CCFbxFactory_h_21_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define C__Users_allenlee_BEYOUNG_Desktop_Unreal_4_24_RLLiveLink_RLLiveLink_RLPlugin_HostProject_Plugins_RLPlugin_Source_RLPlugin_Private_CCFbxFactory_h_21_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	C__Users_allenlee_BEYOUNG_Desktop_Unreal_4_24_RLLiveLink_RLLiveLink_RLPlugin_HostProject_Plugins_RLPlugin_Source_RLPlugin_Private_CCFbxFactory_h_21_PRIVATE_PROPERTY_OFFSET \
+	C__Users_allenlee_BEYOUNG_Desktop_Unreal_4_24_RLLiveLink_RLLiveLink_RLPlugin_HostProject_Plugins_RLPlugin_Source_RLPlugin_Private_CCFbxFactory_h_21_SPARSE_DATA \
+	C__Users_allenlee_BEYOUNG_Desktop_Unreal_4_24_RLLiveLink_RLLiveLink_RLPlugin_HostProject_Plugins_RLPlugin_Source_RLPlugin_Private_CCFbxFactory_h_21_RPC_WRAPPERS_NO_PURE_DECLS \
+	C__Users_allenlee_BEYOUNG_Desktop_Unreal_4_24_RLLiveLink_RLLiveLink_RLPlugin_HostProject_Plugins_RLPlugin_Source_RLPlugin_Private_CCFbxFactory_h_21_INCLASS_NO_PURE_DECLS \
+	C__Users_allenlee_BEYOUNG_Desktop_Unreal_4_24_RLLiveLink_RLLiveLink_RLPlugin_HostProject_Plugins_RLPlugin_Source_RLPlugin_Private_CCFbxFactory_h_21_ENHANCED_CONSTRUCTORS \
+static_assert(false, "Unknown access specifier for GENERATED_BODY() macro in class CCFbxFactory."); \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> RLPLUGIN_API UClass* StaticClass<class UCCFbxFactory>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID C__Users_allenlee_BEYOUNG_Desktop_Unreal_4_24_RLLiveLink_RLLiveLink_RLPlugin_HostProject_Plugins_RLPlugin_Source_RLPlugin_Private_CCFbxFactory_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
---
+++ CC3_Character_Test/Plugins/Auto_Setup_1.03/Intermediate/Build/Win64/UE4Editor/Inc/RLPlugin/CCImportUI.generated.h
@@ -0,0 +1,97 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef RLPLUGIN_CCImportUI_generated_h
+#error "CCImportUI.generated.h already included, missing '#pragma once' in CCImportUI.h"
+#endif
+#define RLPLUGIN_CCImportUI_generated_h
+
+#define C__Users_allenlee_BEYOUNG_Desktop_Unreal_4_24_RLLiveLink_RLLiveLink_RLPlugin_HostProject_Plugins_RLPlugin_Source_RLPlugin_Private_CCImportUI_h_14_SPARSE_DATA
+#define C__Users_allenlee_BEYOUNG_Desktop_Unreal_4_24_RLLiveLink_RLLiveLink_RLPlugin_HostProject_Plugins_RLPlugin_Source_RLPlugin_Private_CCImportUI_h_14_RPC_WRAPPERS
+#define C__Users_allenlee_BEYOUNG_Desktop_Unreal_4_24_RLLiveLink_RLLiveLink_RLPlugin_HostProject_Plugins_RLPlugin_Source_RLPlugin_Private_CCImportUI_h_14_RPC_WRAPPERS_NO_PURE_DECLS
+#define C__Users_allenlee_BEYOUNG_Desktop_Unreal_4_24_RLLiveLink_RLLiveLink_RLPlugin_HostProject_Plugins_RLPlugin_Source_RLPlugin_Private_CCImportUI_h_14_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesUCCImportUI(); \
+	friend struct Z_Construct_UClass_UCCImportUI_Statics; \
+public: \
+	DECLARE_CLASS(UCCImportUI, UObject, COMPILED_IN_FLAGS(0), CASTCLASS_None, TEXT("/Script/RLPlugin"), RLPLUGIN_API) \
+	DECLARE_SERIALIZER(UCCImportUI) \
+	static const TCHAR* StaticConfigName() {return TEXT("EditorPerProjectUserSettings");} \
+
+
+
+#define C__Users_allenlee_BEYOUNG_Desktop_Unreal_4_24_RLLiveLink_RLLiveLink_RLPlugin_HostProject_Plugins_RLPlugin_Source_RLPlugin_Private_CCImportUI_h_14_INCLASS \
+private: \
+	static void StaticRegisterNativesUCCImportUI(); \
+	friend struct Z_Construct_UClass_UCCImportUI_Statics; \
+public: \
+	DECLARE_CLASS(UCCImportUI, UObject, COMPILED_IN_FLAGS(0), CASTCLASS_None, TEXT("/Script/RLPlugin"), RLPLUGIN_API) \
+	DECLARE_SERIALIZER(UCCImportUI) \
+	static const TCHAR* StaticConfigName() {return TEXT("EditorPerProjectUserSettings");} \
+
+
+
+#define C__Users_allenlee_BEYOUNG_Desktop_Unreal_4_24_RLLiveLink_RLLiveLink_RLPlugin_HostProject_Plugins_RLPlugin_Source_RLPlugin_Private_CCImportUI_h_14_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	RLPLUGIN_API UCCImportUI(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(UCCImportUI) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(RLPLUGIN_API, UCCImportUI); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(UCCImportUI); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	RLPLUGIN_API UCCImportUI(UCCImportUI&&); \
+	RLPLUGIN_API UCCImportUI(const UCCImportUI&); \
+public:
+
+
+#define C__Users_allenlee_BEYOUNG_Desktop_Unreal_4_24_RLLiveLink_RLLiveLink_RLPlugin_HostProject_Plugins_RLPlugin_Source_RLPlugin_Private_CCImportUI_h_14_ENHANCED_CONSTRUCTORS \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	RLPLUGIN_API UCCImportUI(UCCImportUI&&); \
+	RLPLUGIN_API UCCImportUI(const UCCImportUI&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(RLPLUGIN_API, UCCImportUI); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(UCCImportUI); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(UCCImportUI)
+
+
+#define C__Users_allenlee_BEYOUNG_Desktop_Unreal_4_24_RLLiveLink_RLLiveLink_RLPlugin_HostProject_Plugins_RLPlugin_Source_RLPlugin_Private_CCImportUI_h_14_PRIVATE_PROPERTY_OFFSET
+#define C__Users_allenlee_BEYOUNG_Desktop_Unreal_4_24_RLLiveLink_RLLiveLink_RLPlugin_HostProject_Plugins_RLPlugin_Source_RLPlugin_Private_CCImportUI_h_11_PROLOG
+#define C__Users_allenlee_BEYOUNG_Desktop_Unreal_4_24_RLLiveLink_RLLiveLink_RLPlugin_HostProject_Plugins_RLPlugin_Source_RLPlugin_Private_CCImportUI_h_14_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	C__Users_allenlee_BEYOUNG_Desktop_Unreal_4_24_RLLiveLink_RLLiveLink_RLPlugin_HostProject_Plugins_RLPlugin_Source_RLPlugin_Private_CCImportUI_h_14_PRIVATE_PROPERTY_OFFSET \
+	C__Users_allenlee_BEYOUNG_Desktop_Unreal_4_24_RLLiveLink_RLLiveLink_RLPlugin_HostProject_Plugins_RLPlugin_Source_RLPlugin_Private_CCImportUI_h_14_SPARSE_DATA \
+	C__Users_allenlee_BEYOUNG_Desktop_Unreal_4_24_RLLiveLink_RLLiveLink_RLPlugin_HostProject_Plugins_RLPlugin_Source_RLPlugin_Private_CCImportUI_h_14_RPC_WRAPPERS \
+	C__Users_allenlee_BEYOUNG_Desktop_Unreal_4_24_RLLiveLink_RLLiveLink_RLPlugin_HostProject_Plugins_RLPlugin_Source_RLPlugin_Private_CCImportUI_h_14_INCLASS \
+	C__Users_allenlee_BEYOUNG_Desktop_Unreal_4_24_RLLiveLink_RLLiveLink_RLPlugin_HostProject_Plugins_RLPlugin_Source_RLPlugin_Private_CCImportUI_h_14_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define C__Users_allenlee_BEYOUNG_Desktop_Unreal_4_24_RLLiveLink_RLLiveLink_RLPlugin_HostProject_Plugins_RLPlugin_Source_RLPlugin_Private_CCImportUI_h_14_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	C__Users_allenlee_BEYOUNG_Desktop_Unreal_4_24_RLLiveLink_RLLiveLink_RLPlugin_HostProject_Plugins_RLPlugin_Source_RLPlugin_Private_CCImportUI_h_14_PRIVATE_PROPERTY_OFFSET \
+	C__Users_allenlee_BEYOUNG_Desktop_Unreal_4_24_RLLiveLink_RLLiveLink_RLPlugin_HostProject_Plugins_RLPlugin_Source_RLPlugin_Private_CCImportUI_h_14_SPARSE_DATA \
+	C__Users_allenlee_BEYOUNG_Desktop_Unreal_4_24_RLLiveLink_RLLiveLink_RLPlugin_HostProject_Plugins_RLPlugin_Source_RLPlugin_Private_CCImportUI_h_14_RPC_WRAPPERS_NO_PURE_DECLS \
+	C__Users_allenlee_BEYOUNG_Desktop_Unreal_4_24_RLLiveLink_RLLiveLink_RLPlugin_HostProject_Plugins_RLPlugin_Source_RLPlugin_Private_CCImportUI_h_14_INCLASS_NO_PURE_DECLS \
+	C__Users_allenlee_BEYOUNG_Desktop_Unreal_4_24_RLLiveLink_RLLiveLink_RLPlugin_HostProject_Plugins_RLPlugin_Source_RLPlugin_Private_CCImportUI_h_14_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> RLPLUGIN_API UClass* StaticClass<class UCCImportUI>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID C__Users_allenlee_BEYOUNG_Desktop_Unreal_4_24_RLLiveLink_RLLiveLink_RLPlugin_HostProject_Plugins_RLPlugin_Source_RLPlugin_Private_CCImportUI_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
